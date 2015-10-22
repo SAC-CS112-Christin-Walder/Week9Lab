4f78d8e081895516c9b0e13878721b9ab1cc8edf
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-10-21 19:29:12 -0700

    Lab1

---
+++ src/Lab1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		
+		int[] aryNums;
+		aryNums = new int[6];
+		double avg = 0;
+		
+		aryNums[0] = 123;
+		aryNums [1] = 456;
+		aryNums [2] = 479;
+		aryNums [3] = 135;
+		aryNums [4] = 246;
+		aryNums [5] = 135;
+		
+		Arrays.sort(aryNums);
+		
+		int i;
+		
+		for (i=0; i<aryNums.length; i++) {
+			System.out.println("Number: " + aryNums[i]); }
+			
+		avg = ((aryNums[0]+aryNums[1]+aryNums[2]+aryNums[3]+aryNums[4]+aryNums[5])/i);
+		
+		System.out.printf("%.2f", avg);
+			
+	}
+}
